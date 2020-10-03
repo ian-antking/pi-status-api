999c68101faebf1d4340f2eca8e6469c341bacc4
ian-antking <ian.antking@gmail.com>
2020-10-03 20:39:15 +0100

    add tests for buildStatusMessage

---
+++ __tests__/build-status-message.test.js
@@ -0,0 +1,27 @@
+const buildStatusMessage = require('../src/helpers/build-status-message');
+
+describe('buildStatusMessage', () => {
+  it('sets the message mode to solid', () => {
+    const message = buildStatusMessage('busy');
+    expect(message.mode).toBe('solid');
+  });
+
+  it('sets the color to red, when busy', () => {
+    const message = buildStatusMessage('busy');
+    expect(message.color).toBe('(255, 0, 0)');
+  });
+
+  it('sets the color to green, when free', () => {
+    const message = buildStatusMessage('free');
+    expect(message.color).toBe('(0, 255, 0)');
+  });
+
+  it('sets the color to amber, when free', () => {
+    const message = buildStatusMessage('working');
+    expect(message.color).toBe('(255, 168, 0)');
+  });
+
+  it('throws an error if state is not free, working or busy', () => {
+    expect(() => buildStatusMessage('foo')).toThrow('invalid argument: state must be free, working or busy');
+  });
+});
