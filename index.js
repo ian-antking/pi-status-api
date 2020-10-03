ba240dbba328ab44aaa218026183cd87615646a8
ian-antking <ian.antking@gmail.com>
2020-10-03 15:14:51 +0100

    add esling and async-mqtt

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const app = require('./src/app');
 
 const port = process.env.PORT || 3000;
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`app listening on ${port}`));
