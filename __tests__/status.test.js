6ab897eb3dc2f2be4172441c987fe52aff0414b9
ian-antking <ian.antking@gmail.com>
2020-10-04 00:20:54 +0100

    test free, working and busy messages

--- __tests__/status.test.js
+++ __tests__/status.test.js
@@ -1,21 +1,49 @@
 const request = require('supertest');
 const faker = require('faker');
+const mqtt = require('../src/services/mqtt');
 const app = require('../src/app');
 
 jest.mock('../src/services/mqtt');
 
 describe('GET /status', () => {
-  it('returns a 200 status, the published message and the topic', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('publishes a busy message to correct topic and returns 200', async () => {
     const name = faker.name.firstName().toLowerCase();
+    const expectedMessage = { color: '(255, 0, 0)', mode: 'solid' };
     const response = await request(app)
       .get(`/status/${name}/busy`);
 
+    expect(mqtt.publish).toHaveBeenCalledWith(`status/${name}`, expectedMessage);
     expect(response.status).toBe(200);
     expect(response.body.topic).toBe(`status/${name}`);
-    expect(response.body.message).toEqual({
-      color: '(255, 0, 0)',
-      mode: 'solid',
-    });
+    expect(response.body.message).toEqual(expectedMessage);
+  });
+
+  it('publishes a working message to correct topic and returns 200', async () => {
+    const name = faker.name.firstName().toLowerCase();
+    const expectedMessage = { color: '(255, 168, 0)', mode: 'solid' };
+    const response = await request(app)
+      .get(`/status/${name}/working`);
+
+    expect(mqtt.publish).toHaveBeenCalledWith(`status/${name}`, expectedMessage);
+    expect(response.status).toBe(200);
+    expect(response.body.topic).toBe(`status/${name}`);
+    expect(response.body.message).toEqual(expectedMessage);
+  });
+
+  it('publishes a working message to correct topic and returns 200', async () => {
+    const name = faker.name.firstName().toLowerCase();
+    const expectedMessage = { color: '(0, 255, 0)', mode: 'solid' };
+    const response = await request(app)
+      .get(`/status/${name}/free`);
+
+    expect(mqtt.publish).toHaveBeenCalledWith(`status/${name}`, expectedMessage);
+    expect(response.status).toBe(200);
+    expect(response.body.topic).toBe(`status/${name}`);
+    expect(response.body.message).toEqual(expectedMessage);
   });
 
   it('returns an error if state is invalid', async () => {
@@ -24,6 +52,7 @@ describe('GET /status', () => {
     const response = await request(app)
       .get(`/status/${name}/${state}`);
 
+    expect(mqtt.publish).not.toHaveBeenCalled();
     expect(response.status).toBe(401);
     expect(response.body.error).toBe('invalid argument: state must be free, working or busy');
     expect(response.body.request).toEqual({ name, state });
