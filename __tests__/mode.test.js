a95cc3344b8fbd045e3cac20a6e294d67b3c9197
ian-antking <ian.antking@gmail.com>
2020-10-04 00:40:31 +0100

    add test generator for repetative tests

--- __tests__/mode.test.js
+++ __tests__/mode.test.js
@@ -6,16 +6,29 @@ const app = require('../src/app');
 jest.mock('../src/services/mqtt');
 
 describe('GET /mode/:name/:mode', () => {
-  it('publishes a mode message to the status topic', async () => {
-    const name = faker.name.firstName().toLowerCase();
-    const response = await request(app)
-      .get(`/mode/${name}/off`);
+  const modes = [
+    'off',
+    'solid',
+    'blink',
+    'rainbow',
+    'alert',
+  ];
 
-    expect(mqtt.publish).toHaveBeenCalled();
-    expect(response.status).toBe(200);
-    expect(response.body.topic).toBe(`status/${name}`);
-    expect(response.body.message).toEqual({
-      mode: 'off',
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  modes.forEach((mode) => {
+    it(`publishes a "${mode}" message`, async () => {
+      const name = faker.name.firstName().toLowerCase();
+      const expectedMessage = { mode };
+      const response = await request(app)
+        .get(`/mode/${name}/${mode}`);
+
+      expect(mqtt.publish).toHaveBeenCalledWith(`status/${name}`, expectedMessage);
+      expect(response.status).toBe(200);
+      expect(response.body.topic).toBe(`status/${name}`);
+      expect(response.body.message).toEqual(expectedMessage);
     });
   });
 });
