7c11ea46138b4845676595fb3950b6434a458f31
ian-antking <ian.antking@gmail.com>
2020-10-03 23:44:10 +0100

    combine assigne statements into single destructure

--- src/controllers/status.js
+++ src/controllers/status.js
@@ -2,13 +2,12 @@ const { publish } = require('../services/mqtt');
 const buildStatusMessage = require('../helpers/build-status-message');
 
 exports.set = async (req, res) => {
-  const { name } = req.params;
-  const { state } = req.params;
+  const { name, state } = req.params;
   const topic = `status/${name}`;
 
   try {
     const message = buildStatusMessage(state);
-    await publish(topic, JSON.stringify(message));
+    await publish(topic, message);
     res.status(200).json({ topic, message });
   } catch (error) {
     res.status(401).json({
