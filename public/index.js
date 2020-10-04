975769834141b4b603db700cbb7a3eb19881efa1
ian-antking <ian.antking@gmail.com>
2020-10-04 14:18:33 +0100

    alert user if no name is entered

--- public/index.js
+++ public/index.js
@@ -1,10 +1,19 @@
+/* eslint-disable no-alert */
 /* eslint-disable no-unused-vars */
 function setStatus(status) {
   const name = document.getElementById('name-input').value;
-  window.fetch(`http://${window.location.host}/status/${name}/${status}`);
+  if (!name) {
+    window.alert('please enter your name');
+  } else {
+    window.fetch(`http://${window.location.host}/status/${name}/${status}`);
+  }
 }
 
 function setMode(mode) {
   const name = document.getElementById('name-input').value;
-  window.fetch(`http://${window.location.host}/mode/${name}/${mode}`);
+  if (!name) {
+    window.alert('please enter your name');
+  } else {
+    window.fetch(`http://${window.location.host}/mode/${name}/${mode}`);
+  }
 }
